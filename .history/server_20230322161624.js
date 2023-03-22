6b12fdf8914933a823d3ec0ca3bd7df51ced4861
Adrian Chrysanthou <adrianvfx@gmail.com>
2023-03-22 17:09:24 -0400

    Initial commit

---
+++ .history/server_20230322161624.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require('dotenv');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+
+dotenv.config();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Initialize the SQLite database
+const db = new sqlite3.Database(':memory:');
+
+const openai = new OpenAIApi(configuration);
+
+db.serialize(() => {
+  db.run('CREATE TABLE conversations (id TEXT, role TEXT, content TEXT)');
+});
+
+
+function generatePrompt(conversationId, userMessage) {
+  return new Promise(async (resolve) => {
+    const conversationHistory = await getConversationHistory(conversationId);
+    conversationHistory.push({ role: 'user', content: userMessage });
+
+    let prompt = 'You are an AI-powered guessing game similar to Akinator. Your goal is to guess the character, object, or animal based on the user\'s answers to your questions.\n\n';
+
+    conversationHistory.forEach((message) => {
+      prompt += `${message.role === 'user' ? 'User' : 'AI'}: ${message.content}\n`;
+    });
+
+    resolve(prompt);
+  });
+}
+
+function getConversationHistory(conversationId) {
+  return new Promise((resolve) => {
+    db.all('SELECT * FROM conversations WHERE id = ?', conversationId, (err, rows) => {
+      if (err) {
+        console.error(err);
+        resolve([]);
+      } else {
+        resolve(rows.map(row => ({ role: row.role, content: row.content })));
+      }
+    });
+  });
+}
+
+function saveMessage(conversationId, role, content) {
+  db.run('INSERT INTO conversations (id, role, content) VALUES (?, ?, ?)', [conversationId, role, content], (err) => {
+    if (err) console.error(err);
+  });
+}
+
+
+app.post('/continue-conversation', async (req, res) => {
+  try {
+    const { conversationId, userInput } = req.body;
+
+    if (!conversationId) {
+      return res.status(400).json({ error: 'Conversation ID is required' });
+    }
+
+    saveMessage(conversationId, 'user', userInput);
+
+    const prompt = await generatePrompt(conversationId, userInput);
+
+     // Check conversation history length
+     const conversationHistory = await getConversationHistory(conversationId);
+     if (conversationHistory.length >= 20) {
+       const aiMessage = "You've stumped me, let's try again!";
+       saveMessage(conversationId, 'ai', aiMessage);
+       return res.status(200).json({ message: aiMessage });
+     }
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: "user", content: prompt}],
+      stop: ["You:"]
+    });
+     // Concatenate the content from each chunk
+     console.log(response.data)
+     const aiMessage = response.data.choices.map(choice => choice.message.content || '').join('').trim();
+    saveMessage(conversationId, 'ai', aiMessage);
+
+    res.status(200).json({ message: aiMessage });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
+});
+
+const PORT = process.env.PORT || 5890;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
